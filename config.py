37e6b78896a017b8143cf4fa20ad14c6abb6ea92
Amitsharma210 <144205854+Amitsharma210@users.noreply.github.com>
2024-07-23 07:15:55 +0530

    Update config.py

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ from os import getenv
 
 API_ID = int(getenv("API_ID", "26406803"))
 API_HASH = getenv("API_HASH", "bee8853d1219e889f6ce5452dc9ef842")
-BOT_TOKEN = getenv("BOT_TOKEN", "7355250433:AAFjyMpTQ_doznZMcxAy-ZCjZ2z6AHzXx7k")
+BOT_TOKEN = getenv("BOT_TOKEN", "7289472677:AAEyCzOOGtglSUhJEo_eTG_WdHUWYo9PnY4")
 OWNER_ID = int(getenv("OWNER_ID", "7250912826"))
 MONGODB_CONNECTION_STRING = getenv("MONGO_DB", "mongodb+srv://eraon8:d0DiBsv96iEW8JSb@rameshlal.errc5t2.mongodb.net/?retryWrites=true&w=majority&appName=Rameshlal")
 LOG_GROUP = int(getenv("LOG_GROUP", "-1002186478269"))
